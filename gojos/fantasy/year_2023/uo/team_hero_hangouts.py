07e9930ca9e90d3fccf1927e2647ceecc849a2c5
Juki <jude@fauve.io>
2023-06-17 18:58:22 +1200

    Update team_hero_hangouts.py

--- gojos/fantasy/year_2023/uo/team_hero_hangouts.py
+++ gojos/fantasy/year_2023/uo/team_hero_hangouts.py
@@ -42,6 +42,11 @@ def wildcards(major):
 
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(Scheffler).trade_in(Morikawa))
     """
+
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(Taylor).trade_in(Fowler))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(Rahm).trade_in(English))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(Fleetwood).trade_in(Woo_Lee))
+    
     pass
 
 
