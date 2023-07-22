8426881f4477e9fd1d71c24bfa4e4c353bb5280b
Juki <jude@fauve.io>
2023-07-23 00:12:09 +1200

    Update bear_necessities.py

--- gojos/fantasy/year_2023/to/bear_necessities.py
+++ gojos/fantasy/year_2023/to/bear_necessities.py
@@ -46,4 +46,10 @@ def wildcards(major):
 
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Scheffler).trade_in(players.Morikawa))
     """
+
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.An).trade_in(players.Day))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Lowry).trade_in(players.Lee_MW))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.MacIntyre).trade_in(players.Harman))
+
+    
     pass
