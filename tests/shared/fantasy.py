1b3229baa77e3a1fba47ef5b9fbf1e8788d19802
Col Perks <wild.fauve@gmail.com>
2023-07-22 18:22:55 +1200

    updates

--- tests/shared/fantasy.py
+++ tests/shared/fantasy.py
@@ -5,6 +5,6 @@ from gojos import model
 
 @pytest.fixture
 def test_teams():
-    model.Team.create("Bear Necessities", "a, b, c")
-    model.Team.create("Clojos", "d, e, f")
-    model.Team.create("Fauve", "d, e, f")
+    return (model.Team.create("Bear Necessities", "a, b, c"),
+            model.Team.create("Clojos", "d, e, f"),
+            model.Team.create("Fauve", "d, e, f"))
