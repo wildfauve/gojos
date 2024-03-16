f1e5219fa95e2a7f78953607dd9d0ba8090d7687
Col Perks <wild.fauve@gmail.com>
2024-03-17 07:35:54 +1300

    update missing names

--- gojos/fantasy/year_2024/pl/fauve.py
+++ gojos/fantasy/year_2024/pl/fauve.py
@@ -48,6 +48,6 @@ def wildcards(major):
 
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Scheffler).trade_in(players.Morikawa))
     """
-    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Spieth).trade_in(players.Fitzpatrick))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Spieth).trade_in(players.Fitzpatrick_M))
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.An).trade_in(players.Theegala))
     pass
--- gojos/fantasy/year_2024/pl/light_house.py
+++ gojos/fantasy/year_2024/pl/light_house.py
@@ -48,7 +48,7 @@ def wildcards(major):
 
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Scheffler).trade_in(players.Morikawa))
     """
-    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Zalatoris).trade_in(players.Taylor))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Zalatoris).trade_in(players.Taylor_N))
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Thomas).trade_in(players.Schauffele))
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Davis).trade_in(players.Day))
     pass
