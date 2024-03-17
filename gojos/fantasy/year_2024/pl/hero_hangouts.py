f22ee10525944200a90146acd5c67c4141a848ef
Juki <jude@fauve.io>
2024-03-17 13:06:35 +1300

    Update hero_hangouts.py

--- gojos/fantasy/year_2024/pl/hero_hangouts.py
+++ gojos/fantasy/year_2024/pl/hero_hangouts.py
@@ -50,6 +50,8 @@ def wildcards(major):
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Scheffler).trade_in(players.Morikawa))
     """
 
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Fox).trade_in(players.Harman))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Day).trade_in(players.McNealy))
     pass
 
 
