cf05b9fea0dc9a31309e7db5f29dfc72337df0da
Juki <jude@fauve.io>
2024-04-14 12:45:18 +1200

    Update bear_necessities.py

--- gojos/fantasy/year_2024/ms/bear_necessities.py
+++ gojos/fantasy/year_2024/ms/bear_necessities.py
@@ -46,6 +46,8 @@ def wildcards(major):
 
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Scheffler).trade_in(players.Morikawa))
     """
-
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.McIlroy).trade_in(players.Homa))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Lowry).trade_in(players.Davis))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Bhatia).trade_in(players.Morikawa))
     
     pass
