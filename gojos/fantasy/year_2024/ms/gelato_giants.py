d82b8a503ee8e8e0de693cab2376b3660a2ab6dc
Juki <jude@fauve.io>
2024-04-14 12:49:30 +1200

    Update gelato_giants.py

--- gojos/fantasy/year_2024/ms/gelato_giants.py
+++ gojos/fantasy/year_2024/ms/gelato_giants.py
@@ -50,4 +50,8 @@ def wildcards(major):
     TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Scheffler).trade_in(players.Morikawa))
     """
 
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Hovland).trade_in(players.Fox))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Lowry).trade_in(players.Davis))
+    TEAM.major(major).play_wildcard(WildCard().from_round(3).trade_out(players.Bhatia).trade_in(players.Morikawa))
+
     pass
